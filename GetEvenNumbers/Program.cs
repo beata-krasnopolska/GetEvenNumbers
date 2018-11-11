e17941aaba23a79742b7090bbc022e2ba3d9f239
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-11-11 11:39:08 +0100

    Add project files.

---
+++ GetEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace GetEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The numbers which produce reminder 0 after divided by 2 are:");
+            int[] table = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var even = from numbers in table
+                       where (numbers % 2 == 0)
+                       select numbers;
+
+            foreach (var numbers in even)
+            {
+                Console.WriteLine("{0}", numbers);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
